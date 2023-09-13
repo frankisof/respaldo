b8d8fe8046231217820e33370a8e5e78aabf50d5
Francisco <fernandezfrancisco261@gmail.com>
2023-09-12 21:43:45 -0600

    skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
