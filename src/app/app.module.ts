eae3624c4e0891cc659df3946f17b5a807fa6436
Francisco <fernandezfrancisco261@gmail.com>
2023-09-13 19:32:11 -0600

    deccoracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { InterestsComponent } from './interests/interests.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    WorkExperienceComponent,
+    SkillsComponent,
+    CertificationsComponent,
+    InterestsComponent
+    
+
+  
   ],
   imports: [
     BrowserModule,
